5ce00acf40bd04ac1f08a35399ae0aa4bd42b5b1
Marian <marianmoh21@gmail.com>
2024-02-07 15:57:59 +0200

    Add unit tests for User class

---
+++ tests/UserTest.java
@@ -0,0 +1,98 @@
+
+import org.junit.*;
+import java.io.*;
+
+public class UserTest {
+    private static User user;
+
+    @BeforeClass
+    public static void setUpBeforeClass() {
+        user = new User("Marin", 1234);
+        user.setStatus(Status.ADMIN);
+    }
+
+    @Test
+    public void testWriteUserToFile() {
+        user.writeUserToFile();
+
+        StringBuilder text = new StringBuilder();
+        try (BufferedReader br = new BufferedReader(new FileReader(user.getFileName()))) {
+            String line;
+            int i = 1;
+            while ((line = br.readLine()) != null) {
+                if(i == 1) {
+                    text = new StringBuilder();
+                }
+                text.append(line).append("\n");
+                i = (i % 5) + 1;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String expectedText = "\n" + user.getName()
+                + "\n" + user.getPassword()
+                + "\n" + user.getStatus()
+                + "\n" + user.getLastTimeOnline()
+                + "\n";
+        Assert.assertEquals(expectedText, text.toString());
+    }
+
+    @Test
+    public void testReadInfoFromFile() {
+        try {
+            FileWriter fw = new FileWriter(user.getFileName(), false);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        user.writeUserToFile();
+        String text = User.readInfoFromFile(false);
+
+        String expectedText = "\n" + user.getName()
+                + "\n" + user.getPassword()
+                + "\n" + user.getStatus()
+                + "\n" + user.getLastTimeOnline()
+                + "\n";
+        Assert.assertEquals(expectedText, text);
+    }
+
+    @Test
+    public void testReadFullInfoFromFile() {
+        try {
+            FileWriter fw = new FileWriter(user.getFileName(), false);
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        user.writeUserToFile();
+        String text = User.readInfoFromFile(true);
+
+        String expectedText = "\nName: " + user.getName()
+                + "\nPassword: " + user.getPassword()
+                + "\nStatus: " + user.getStatus()
+                + "\nLast time online: " + user.getLastTimeOnline()
+                + "\n";
+        Assert.assertEquals(expectedText, text);
+    }
+
+    @Test
+    public void testDeleteInfoFromFile() {
+        User.deleteInfoFromFile();
+        StringBuilder text = new StringBuilder();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(user.getFileName()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        String expectedText = "";
+        Assert.assertEquals(expectedText, text.toString());
+    }
+}
